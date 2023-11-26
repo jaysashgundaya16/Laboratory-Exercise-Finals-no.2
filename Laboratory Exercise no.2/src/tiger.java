caea2dc0392b4a77eb0a7d10ebf48fe2db91f406
jaysashgundaya16 <jaysashgundaya17@gmail.com>
2023-11-26 09:02:08 +0800

    change and add my cat class

--- Laboratory Exercise no.2/src/tiger.java
+++
@@ -1,9 +0,0 @@
-package laboratoryexercisefinalsno2;
-
-public class Tiger extends Prey {
-    
-    @Override
-    public void Color(){
-        System.out.println("biggest kitten in the World");
-    }
-}
