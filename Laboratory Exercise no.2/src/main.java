c9e8a45ca0db5d50646a0e06849e06688ade6109
jaysashgundaya16 <jaysashgundaya17@gmail.com>
2023-11-26 08:11:25 +0800

    First commit in Finals no.2

---
+++ Laboratory Exercise no.2/src/main.java
@@ -0,0 +1,14 @@
+package laboratoryexercisefinalsno2;
+
+public class Main {
+    public static void main(String[] args){
+        
+        Lion lion = new Lion();
+        lion.Lion();
+        lion.Color();
+        
+        Tiger tiger = new Tiger();
+        tiger.Tiger();
+        tiger.Color();
+    }
+}
